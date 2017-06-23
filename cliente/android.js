4ecb216b81c2ac7fa08a991dcdaa20d5d33568b5
Milton Quirino <miltonquirino.br@gmail.com>
2017-06-23 18:15:13 -0300

    add request json

---
+++ cliente/android.js
@@ -0,0 +1,36 @@
+
+var http = require('http');
+
+var opcoes = {
+  hostname: 'localhost',
+  port: 8787,
+  path: '/',
+  method: 'post',
+  headers: {
+		'Accept' : 'application/json',
+		'Content-type' : 'application/json'
+	}
+}
+
+//Content Type
+var html = 'nome=José'; //x-www-form-urlencoded
+var json = { nome:'Jose' };
+var string_json = JSON.stringify(json);
+
+var buffer_corpo_response = [];
+
+var req = http.request(opcoes, function(res){
+
+  res.on('data', function(pedaco){
+    buffer_corpo_response.push(pedaco);
+  });
+
+  res.on('end',function(){
+    var corpo_response =  Buffer.concat(buffer_corpo_response).toString();
+    console.log(corpo_response);
+  });
+
+});
+
+req.write(string_json);
+req.end();
